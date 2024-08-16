95a920e2f9186ce7805167a84f913ac24280f573
lid9 <valyngo07@gmail.com>
2024-08-16 14:30:42 -0700

    admin dashboard

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -15,14 +15,17 @@ import { FormFieldType } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-  userId, patientId, type
+  userId, patientId, type, appointment, setOpen
 }: {
   userId: string; 
   patientId: string; 
   type: "create" | "cancel" | "schedule";
+  appointment?: Appointment; 
+  setOpen: (open: boolean) => void; 
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -32,15 +35,17 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment.primaryPhysician : '',
+      schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+      reason: appointment ? appointment.reason : '', 
+      note: appointment?.note || '',
+      cancellationReason: appointment?.cancellationReason || '',
     },
   })
 
   async function onSubmit(values: z.infer<typeof AppointmentFormValidation>) {
+    console.log('IM SUBMITTING', {type})
+
     setIsLoading(true);
 
     let status;
@@ -55,13 +60,9 @@ const AppointmentForm = ({
         status = 'pending';
         break;
     }
-
-    console.log('BEFORE THE TYPE', type)
-
+console.log({type})
     try {
       if(type === 'create' && patientId) {
-        console.log('IM HERE')
-
         const appointmentData = {
           userId,
           patient: patientId,
@@ -74,11 +75,29 @@ const AppointmentForm = ({
 
         const appointment = await createAppointment(appointmentData);
 
-        console.log(appointment)
-
         if(appointment) {
           form.reset();
           router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+        }
+      } else {
+        console.log('Updating appointment')
+        const appointmentToUpdate = {
+          userId, 
+          appointmentId: appointment?.$id!, 
+          appointment: {
+            primaryPhysician: values?.primaryPhysician, 
+            schedule: new Date(values?.schedule), 
+            status: status as Status, 
+            cancellationReason: values?.cancellationReason,
+          }, 
+          type
+        }
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate); 
+
+        if(updatedAppointment) {
+          setOpen && setOpen(false); 
+          form.reset(); 
         }
       }
 
@@ -106,10 +125,10 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
+        {type === 'create' && <section className="mb-12 space-y-4">
           <h1 className="header">New Appointment</h1>
           <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-        </section>
+        </section>}
 
         {type !== "cancel" && (
           <>
